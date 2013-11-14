2413bd72a1f5d02c476d8b6048ea873c960fa452
wangwy1105 <wangwy1105@gmail.com>
2013-11-14 15:20:48 -0500

    removed error in baseline code and removed previous test messages

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerChoiceCandAnsSimilarityScorer.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerChoiceCandAnsSimilarityScorer.java
@@ -20,131 +20,116 @@ import edu.cmu.lti.qalab.types.TestDocument;
 import edu.cmu.lti.qalab.utils.Utils;
 
 public class AnswerChoiceCandAnsSimilarityScorer extends JCasAnnotator_ImplBase {
-  // test WENYI
 
-	int K_CANDIDATES = 5;
+  int K_CANDIDATES = 5;
 
-	@Override
-	public void initialize(UimaContext context)
-			throws ResourceInitializationException {
-		super.initialize(context);
-		K_CANDIDATES=(Integer)context.getConfigParameterValue("K_CANDIDATES");
-	}
+  @Override
+  public void initialize(UimaContext context) throws ResourceInitializationException {
+    super.initialize(context);
+    K_CANDIDATES = (Integer) context.getConfigParameterValue("K_CANDIDATES");
+  }
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
-		// String testDocId = testDoc.getId();
-		ArrayList<QuestionAnswerSet> qaSet = Utils
-				.getQuestionAnswerSetFromTestDocCAS(aJCas);
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    TestDocument testDoc = Utils.getTestDocumentFromCAS(aJCas);
+    // String testDocId = testDoc.getId();
+    ArrayList<QuestionAnswerSet> qaSet = Utils.getQuestionAnswerSetFromTestDocCAS(aJCas);
 
-		for (int i = 0; i < qaSet.size(); i++) {
+    for (int i = 0; i < qaSet.size(); i++) {
 
-			Question question = qaSet.get(i).getQuestion();
-			System.out.println("Question: " + question.getText());
-			ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet
-					.get(i).getAnswerList(), Answer.class);
-			//callie
-			for (int ind=choiceList.size()-1; ind>=0; ind--) {
-			  Answer temp = choiceList.get(ind);
-			  if(temp.getIsDiscard()) {
-			    choiceList.remove(ind);
-			  }
-			}
-			ArrayList<CandidateSentence> candSentList = Utils
-					.fromFSListToCollection(qaSet.get(i)
-							.getCandidateSentenceList(),
-							CandidateSentence.class);
+      Question question = qaSet.get(i).getQuestion();
+      System.out.println("Question: " + question.getText());
+      ArrayList<Answer> choiceList = Utils.fromFSListToCollection(qaSet.get(i).getAnswerList(),
+              Answer.class);
+      // callie
+      for (int ind = choiceList.size() - 1; ind >= 0; ind--) {
+        Answer temp = choiceList.get(ind);
+        if (temp.getIsDiscard()) {
+          choiceList.remove(ind);
+        }
+      }
+      ArrayList<CandidateSentence> candSentList = Utils.fromFSListToCollection(qaSet.get(i)
+              .getCandidateSentenceList(), CandidateSentence.class);
 
-			int topK = Math.min(K_CANDIDATES, candSentList.size());
-			for (int c = 0; c < topK; c++) {
+      int topK = Math.min(K_CANDIDATES, candSentList.size());
+      for (int c = 0; c < topK; c++) {
 
-				CandidateSentence candSent = candSentList.get(c);
+        CandidateSentence candSent = candSentList.get(c);
 
-				ArrayList<NounPhrase> candSentNouns = Utils
-						.fromFSListToCollection(candSent.getSentence()
-								.getPhraseList(), NounPhrase.class);
-				ArrayList<NER> candSentNers = Utils.fromFSListToCollection(
-						candSent.getSentence().getNerList(), NER.class);
+        ArrayList<NounPhrase> candSentNouns = Utils.fromFSListToCollection(candSent.getSentence()
+                .getPhraseList(), NounPhrase.class);
+        ArrayList<NER> candSentNers = Utils.fromFSListToCollection(candSent.getSentence()
+                .getNerList(), NER.class);
 
-				ArrayList<CandidateAnswer> candAnsList = new ArrayList<CandidateAnswer>();
-				for (int j = 0; j < choiceList.size(); j++) {
+        ArrayList<CandidateAnswer> candAnsList = new ArrayList<CandidateAnswer>();
+        for (int j = 0; j < choiceList.size(); j++) {
 
-					Answer answer = choiceList.get(j);
-					ArrayList<NounPhrase> choiceNouns = Utils
-							.fromFSListToCollection(answer.getNounPhraseList(),
-									NounPhrase.class);
-					ArrayList<NER> choiceNERs = Utils.fromFSListToCollection(
-							answer.getNerList(), NER.class);
+          Answer answer = choiceList.get(j);
+          ArrayList<NounPhrase> choiceNouns = Utils.fromFSListToCollection(
+                  answer.getNounPhraseList(), NounPhrase.class);
+          ArrayList<NER> choiceNERs = Utils.fromFSListToCollection(answer.getNerList(), NER.class);
 
-					int nnMatch = 0;
-					for (int k = 0; k < candSentNouns.size(); k++) {
-						for (int l = 0; l < choiceNERs.size(); l++) {
-							if (candSentNouns.get(k).getText()
-									.contains(choiceNERs.get(l).getText())) {
-								nnMatch++;
-							}
-						}
-						for (int l = 0; l < choiceNouns.size(); l++) {
-							if (candSentNouns.get(k).getText()
-									.contains(choiceNouns.get(l).getText())) {
-								nnMatch++;
-							}
-						}
-					}
+          int nnMatch = 0;
+          for (int k = 0; k < candSentNouns.size(); k++) {
+            for (int l = 0; l < choiceNERs.size(); l++) {
+              if (candSentNouns.get(k).getText().contains(choiceNERs.get(l).getText())) {
+                nnMatch++;
+              }
+            }
+            for (int l = 0; l < choiceNouns.size(); l++) {
+              if (candSentNouns.get(k).getText().contains(choiceNouns.get(l).getText())) {
+                nnMatch++;
+              }
+            }
+          }
 
-					for (int k = 0; k < candSentNers.size(); k++) {
-						for (int l = 0; l < choiceNERs.size(); l++) {
-							if (candSentNers.get(k).getText()
-									.contains(choiceNERs.get(l).getText())) {
-								nnMatch++;
-							}
-						}
-						for (int l = 0; l < choiceNouns.size(); l++) {
-							if (candSentNers.get(k).getText()
-									.contains(choiceNouns.get(l).getText())) {
-								nnMatch++;
-							}
-						}
+          // Wenyi modified this part
+          // removed some error in the baseline code
+          for (int k = 0; k < candSentNers.size(); k++) {
+            for (int l = 0; l < choiceNERs.size(); l++) {
+              if (candSentNers.get(k).getText().contains(choiceNERs.get(l).getText())) {
+                nnMatch++;
+              }
+            }
+            for (int l = 0; l < choiceNouns.size(); l++) {
+              if (candSentNers.get(k).getText().contains(choiceNouns.get(l).getText())) {
+                nnMatch++;
+              }
+            }
 
-					}
+          }
 
-					System.out.println(choiceList.get(j).getText() + "\t"
-							+ nnMatch);
-					CandidateAnswer candAnswer = null;
-					if (candSent.getCandAnswerList() == null) {
-						candAnswer = new CandidateAnswer(aJCas);
-					} else {
-						candAnswer = Utils.fromFSListToCollection(
-								candSent.getCandAnswerList(),
-								CandidateAnswer.class).get(j);// new
-																// CandidateAnswer(aJCas);;
+          System.out.println(choiceList.get(j).getText() + "\t" + nnMatch);
+          CandidateAnswer candAnswer = null;
+          if (candSent.getCandAnswerList() == null) {
+            candAnswer = new CandidateAnswer(aJCas);
+          } else {
+            candAnswer = Utils.fromFSListToCollection(candSent.getCandAnswerList(),
+                    CandidateAnswer.class).get(j);// new
+            // CandidateAnswer(aJCas);;
 
-					}
-					candAnswer.setText(answer.getText());
-					candAnswer.setQId(answer.getQuestionId());
-					candAnswer.setChoiceIndex(j);
-					candAnswer.setSimilarityScore(nnMatch);
-					candAnsList.add(candAnswer);
-				}
+          }
+          candAnswer.setText(answer.getText());
+          candAnswer.setQId(answer.getQuestionId());
+          candAnswer.setChoiceIndex(j);
+          candAnswer.setSimilarityScore(nnMatch);
+          candAnsList.add(candAnswer);
+        }
 
-				FSList fsCandAnsList = Utils.fromCollectionToFSList(aJCas,
-						candAnsList);
-				candSent.setCandAnswerList(fsCandAnsList);
-				candSentList.set(c, candSent);
+        FSList fsCandAnsList = Utils.fromCollectionToFSList(aJCas, candAnsList);
+        candSent.setCandAnswerList(fsCandAnsList);
+        candSentList.set(c, candSent);
 
-			}
+      }
 
-			System.out
-					.println("================================================");
-			FSList fsCandSentList = Utils.fromCollectionToFSList(aJCas,
-					candSentList);
-			qaSet.get(i).setCandidateSentenceList(fsCandSentList);
+      System.out.println("================================================");
+      FSList fsCandSentList = Utils.fromCollectionToFSList(aJCas, candSentList);
+      qaSet.get(i).setCandidateSentenceList(fsCandSentList);
 
-		}
-		FSList fsQASet = Utils.fromCollectionToFSList(aJCas, qaSet);
-		testDoc.setQaList(fsQASet);
+    }
+    FSList fsQASet = Utils.fromCollectionToFSList(aJCas, qaSet);
+    testDoc.setQaList(fsQASet);
 
-	}
+  }
 
 }
