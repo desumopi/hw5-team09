c185640d3e653f297b600bfea6287487f3ea37cd
wangwy1105 <wangwy1105@gmail.com>
2013-11-21 16:14:31 -0500

    added class for cosine similarity

---
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/answer_ranking/AnswerChoiceCandAnsCosSimilarityScorer.java
@@ -0,0 +1,15 @@
+package edu.cmu.lti.deiis.hw5.answer_ranking;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import edu.cmu.lti.qalab.types.Answer;
+import edu.cmu.lti.qalab.types.CandidateAnswer;
+import edu.cmu.lti.qalab.types.CandidateSentence;
+
+public class AnswerChoiceCandAnsCosSimilarityScorer
+{
+
+}
